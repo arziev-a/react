e97f5c0504ac136eade136f3f5ddbc978825ef2d
arziev-a <alinurarziev@gmail.com>
2022-06-15 19:07:33 +0200

    implemented till increment functionality

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,64 @@
+import { data } from "./data.js";
+import Shopping from "./shoppingCart.js";
+import { useState } from "react";
+import "./App.css";
 function App() {
-  return <div>Hello</div>;
+  const [newData, setNewData] = useState(data);
+  const [selectedItems, setSelectedItems] = useState(0);
+  const [totalPrice, setTotalPrice] = useState(0);
+
+  const increment = (id) => {
+    const newArr = [...newData];
+    const foundIndex = newData.findIndex((item) => item.id === id);
+    newArr[foundIndex]["count"]++;
+    setNewData(newArr);
+    let countIt = 0;
+    for (let i of newData) {
+      if (i["count"]) {
+        countIt++;
+      }
+    }
+    setSelectedItems(countIt);
+    let totalP = 0;
+    for (let i of newData) {
+      totalP += i.count * i.price;
+    }
+    setTotalPrice(totalP);
+  };
+  const decrement = (id) => {
+    const newArr = [...newData];
+    const foundIndex = newData.findIndex((item) => item.id === id);
+    if (newArr[foundIndex]["count"]) {
+      newArr[foundIndex]["count"]--;
+    }
+    setNewData(newArr);
+
+    let countIt = 0;
+    for (let i of newData) {
+      if (i["count"]) {
+        countIt++;
+      }
+    }
+    setSelectedItems(countIt);
+  };
+  const countItems = () => {
+    let countIt = 0;
+    for (let i of newData) {
+      if (i["count"]) {
+        countIt++;
+      }
+    }
+    setSelectedItems(countIt);
+  };
+  return (
+    <div className="border main-container mt-5 mb-5">
+      <div className="navbar">Total Items {selectedItems}</div>
+      <Shopping data={newData} increment={increment} decrement={decrement} />
+      <footer className="mb-3 mr-3">
+        <div>Total Price is {totalPrice}$</div>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
---
+++ my-app/src/data.js
@@ -0,0 +1,37 @@
+export const data = [
+  {
+    id: 1,
+    title: "Iphone",
+    price: 299,
+    count: 0,
+    img: "https://www.telepolis.pl/images/katalog/Apple/Apple_iPhone_13_Pro/iphone-13-pro-4.jpg",
+  },
+  {
+    id: 2,
+    title: "Nokia",
+    price: 176,
+    count: 0,
+    img: "https://img.gkbcdn.com/p/2019-01-14/nokia-3110-2-4-inch-mini-phone-blue-1571972672478._w500_.jpg",
+  },
+  {
+    id: 3,
+    title: "motorola",
+    price: 55,
+    count: 0,
+    img: "https://www.notebookcheck.net/uploads/tx_nbc2/4_zu_3_moto_g100.jpg",
+  },
+  {
+    id: 4,
+    title: "sony",
+    price: 150,
+    count: 0,
+    img: "https://f00.esfr.pl/foto/5/41663694809/defde76ccd190cbf1944f12b320725b7/sony-xperia-10-plus-czarny,41663694809_7.jpg",
+  },
+  {
+    id: 5,
+    title: "samsung",
+    price: 233,
+    count: 0,
+    img: "https://images.samsung.com/is/image/samsung/assets/pl/galaxy-a52/buy/galaxy-a52/02_carousel/03_product_kv/a52-5g/A52_5G_AwesomeViolet_ProductKV_MO_img.jpg?imwidth=720",
+  },
+];
